3e9e58618974eb0a425578ca6106912b4a8b5b68
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-11 15:27:21 +0300

    Update 2023_07_11_104440_create_user_photos_table.php

--- project/server/app/database/migrations/2023_07_11_104440_create_user_photos_table.php
+++ project/server/app/database/migrations/2023_07_11_104440_create_user_photos_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_photos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->bigInteger('user_id');
+            $table->binary()
         });
     }
 
