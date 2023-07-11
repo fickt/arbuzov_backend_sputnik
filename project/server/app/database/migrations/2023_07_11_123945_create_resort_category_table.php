08030b0310971834246a2ec46142a0b070fb8a78
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-11 15:43:23 +0300

    categories migration added

---
+++ project/server/app/database/migrations/2023_07_11_123945_create_resort_category_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * intermediate table for many-to-many
+ * resorts >--< categories
+ */
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resort_category', function (Blueprint $table) {
+            $table->bigInteger('resort_id');
+            $table->bigInteger('category_id');
+            $table->primary(['resort_id', 'category_id']);
+
+            $table->foreign('resort_id')
+                ->references('id')
+                ->on('resorts')
+                ->onDelete('cascade');
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resort_category');
+    }
+};
