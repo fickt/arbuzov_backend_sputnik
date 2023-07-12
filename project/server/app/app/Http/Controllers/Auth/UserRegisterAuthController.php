454f5d81673c4a5a2c34cf20ca16f84ab49e889e
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-12 10:34:00 +0300

    routes, userAuthService, registration/login requests added

---
+++ project/server/app/app/Http/Controllers/Auth/UserRegisterAuthController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+class UserRegisterAuthController extends Controller
+{
+    //
+}
--- project/server/app/routes/api.php
+++ project/server/app/routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+/* Authentication */
+/*Route::group(['middleware' => 'api', 'prefix' => 'users'], function () {
+    Route::post('/registration', UserRegisterAuthController::class);
+    Route::post('/login', UserLoginAuthController::class);
+    Route::post('/logout', UserLogoutAuthController::class);
+});*/
