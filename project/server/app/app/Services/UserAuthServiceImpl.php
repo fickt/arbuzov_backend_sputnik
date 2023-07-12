d64d63070a598075cb6cafb75d2ffbb4721f4fab
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-12 11:40:16 +0300

    loginUser and createJwtToken methods implemented

--- project/server/app/app/Services/UserAuthServiceImpl.php
+++ project/server/app/app/Services/UserAuthServiceImpl.php
@@ -6,11 +6,14 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegistrationRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\JsonResponse;
+use Mockery\Exception;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class UserAuthServiceImpl implements UserAuthServiceInterface
 {
-    public function registerUser(RegistrationRequest $request)
+    public function registerUser(RegistrationRequest $request): ResponseFactory
     {
         $user = User::create(
             array_merge(
@@ -19,11 +22,24 @@ class UserAuthServiceImpl implements UserAuthServiceInterface
             )
         );
 
-        return response(new UserResource($user))->setStatusCode(ResponseAlias::HTTP_CREATED);
+        return response(new UserResource($user), ResponseAlias::HTTP_CREATED);
 
     }
 
-    public function loginUser(LoginRequest $request)
+    public function loginUser(LoginRequest $request): JsonResponse
     {
+        if (!$token = auth()->attempt($request->validated())) {
+            throw new Exception('Unauthorized');//UnauthorizedException('Unauthorized');
+        }
+        return $this->createJwtToken($token);
+    }
+
+    private function createJwtToken($token): JsonResponse
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'user' => auth()->user(),
+        ]);
     }
 }
