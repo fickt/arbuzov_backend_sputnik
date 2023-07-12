89ef1f1421563182f4174d430292e83de45b32f7
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-12 11:11:33 +0300

    UserResource added, registration method implemented

--- project/server/app/app/Services/UserAuthServiceImpl.php
+++ project/server/app/app/Services/UserAuthServiceImpl.php
@@ -2,9 +2,29 @@
 
 namespace app\Services;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegistrationRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
 class UserAuthServiceImpl
 {
-    public function registerUser(){
+    public function registerUser(RegistrationRequest $request){
+        $user = User::create(
+            array_merge(
+                $request->validated(),
+                ['password' => bcrypt($request->password)]
+            )
+        );
+        return response()->json(
+            [
+                'user' => new UserResource($user)
+            ],
+            ResponseAlias::HTTP_CREATED
+        );
+    }
+    public function loginUser(LoginRequest $request){
 
     }
 }
