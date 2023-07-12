ddd29470136acb4ee5e65165dc107c2d0a08a4fc
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-12 13:49:41 +0300

    Update User.php

--- project/server/app/app/Models/User.php
+++ project/server/app/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -12,6 +13,14 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * Get the role associated with the user.
+     */
+    public function role(): HasOne
+    {
+        return $this->hasOne(Role::class);
+    }
 
     protected $guarded = [
         'is_blocked',
