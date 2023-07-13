1cf233bdbf71b044fe851c34688fd9e23af01baf
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-13 12:15:03 +0300

    Controller login method implemented

--- project/server/app/app/Http/Controllers/UserController.php
+++ project/server/app/app/Http/Controllers/UserController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegistrationRequest;
 use App\Http\Resources\UserResource;
 
 use App\Models\Role;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Routing\ResponseFactory;
+use Mockery\Exception;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class UserController extends Controller
@@ -32,5 +35,30 @@ class UserController extends Controller
         $userRole = Role::find(self::ROLE_USER_ID);
         $userRole->users()->save($user);
         return response(new UserResource($user), ResponseAlias::HTTP_CREATED);
+    }
+
+    /**
+     * Авторизация пользователя
+     * @param LoginRequest $request - email, password
+     * @return JsonResponse - JWT-token
+     */
+    public function login(LoginRequest $request): JsonResponse {
+        if (!$token = auth()->attempt($request->validated())) {
+            throw new Exception('Unauthorized');//UnauthorizedException('Unauthorized');
+        }
+        return $this->createJwtToken($token);
+    }
+
+    /**
+     * @param $token
+     * @return JsonResponse - JWT-token
+     */
+    private function createJwtToken($token): JsonResponse
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'user' => auth()->user(),
+        ]);
     }
 }
