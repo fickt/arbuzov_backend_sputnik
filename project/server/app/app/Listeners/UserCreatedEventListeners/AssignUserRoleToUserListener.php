c18f9d5e6f9bebae99b328dd4429e9ec4bfcf27b
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-17 15:55:28 +0300

    migration id ->foreignId and onUpdateCascade added

--- project/server/app/app/Listeners/UserCreatedEventListeners/AssignUserRoleToUserListener.php
+++ project/server/app/app/Listeners/UserCreatedEventListeners/AssignUserRoleToUserListener.php
@@ -28,10 +28,6 @@ class AssignUserRoleToUserListener
         $userRole = Role::query()
             ->where('name', '=', Roles::USER)
             ->first();
-       // echo 'lolka';
         $event->getUser()->role()->associate($userRole);
-        //$userRole->users()->save($event->getUser());
-
-      //  $event->getUser()->setRelation('role', $userRole);
     }
 }
