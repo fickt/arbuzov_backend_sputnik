2565ed0febb5d306a880d9f4d0c9fba476df23fa
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-17 20:48:39 +0300

    UserPolicy added

---
+++ server/app/app/Policies/UserPolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Orion\Concerns\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(
+        ?User $user
+    ): Response
+    {
+        return $this->deny();
+    }
+
+    public function view(
+        ?User $user,
+        User  $model
+    ): Response
+    {
+        return $this->deny();
+    }
+
+    public function create(
+        User $user
+    ): Response
+    {
+        return $this->allow();
+    }
+
+    public function update(
+        ?User $user,
+        User  $model
+    ): Response
+    {
+        return $this->deny();
+    }
+
+    public function delete(
+        ?User $user,
+        User  $model
+    ): Response
+    {
+        return $this->deny();
+    }
+
+    public function restore(
+        ?User $user,
+        User  $model
+    ): Response
+    {
+        return $this->deny();
+    }
+
+    public function forceDelete(
+        ?User $user,
+        User  $model
+    ): Response
+    {
+        return $this->deny();
+    }
+
+}
