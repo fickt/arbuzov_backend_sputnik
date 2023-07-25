da32cf08b4de0158bc7f78b04dcf32277df4b15f
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-25 18:38:36 +0300

    ResortRatingPolicy added

--- server/app/app/Http/Controllers/ResortRatingController.php
+++ server/app/app/Http/Controllers/ResortRatingController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ResortRatingRequest;
 use App\Models\ResortRating;
+use App\Policies\ResortRatingPolicy;
 use Orion\Http\Controllers\Controller;
 
 class ResortRatingController extends Controller
 {
     protected $model = ResortRating::class;
     protected $request = ResortRatingRequest::class;
+    protected $policy = ResortRatingPolicy::class;
 }
---
+++ server/app/app/Policies/ResortRatingPolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\ResortRating;
+use App\Models\User;
+use App\Models\UserPhoto;
+use Orion\Concerns\HandlesAuthorization;
+
+class ResortRatingPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(
+        ?User $user
+    ): bool
+    {
+        return $this->authorized()->allowed();
+    }
+
+    public function view(
+        ?User        $user,
+        ResortRating $model
+    ): bool
+    {
+        return $this->authorized()->allowed();
+    }
+
+    public function create(
+        User $user
+    ): bool
+    {
+        return $this->authorized()->allowed();
+    }
+
+    public function update(
+        User         $user,
+        ResortRating $model
+    ): bool
+    {
+        return $this->authorized()->allowed();
+    }
+
+    public function delete(
+        User         $user,
+        ResortRating $model
+    ): bool
+    {
+        return $this->authorized()->allowed();
+    }
+
+    public function restore(
+        User         $user,
+        ResortRating $model
+    ): bool
+    {
+        return $this->authorized()->allowed();
+    }
+
+    public function forceDelete(
+        User         $user,
+        ResortRating $model
+    ): bool
+    {
+        return $this->authorized()->allowed();
+    }
+}
