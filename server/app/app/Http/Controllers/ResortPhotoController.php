a0df6225c64b6093e29aa2073bce8c155b857e69
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-26 13:35:20 +0300

    ResortPhoto added

--- server/app/app/Http/Controllers/ResortPhotoController.php
+++ server/app/app/Http/Controllers/ResortPhotoController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ResortPhoto;
 use Illuminate\Http\Request;
 use Orion\Http\Controllers\Controller;
 
 class ResortPhotoController extends Controller
 {
-
+    protected $model = ResortPhoto::class;
 }
