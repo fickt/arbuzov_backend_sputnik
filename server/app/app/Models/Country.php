752abf4671f1bab8ab89d9e3fb1cf206bcc11c70
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-27 10:12:08 +0300

    Update Country.php

--- server/app/app/Models/Country.php
+++ server/app/app/Models/Country.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
@@ -14,4 +15,9 @@ class Country extends Model
     public $fillable = [
         'name'
     ];
+
+    public function resorts(): HasMany
+    {
+        return $this->hasMany(Resort::class);
+    }
 }
