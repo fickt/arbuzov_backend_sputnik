8d52453af958ea06ede9c4b2d90dd64c413399b2
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-27 10:16:16 +0300

    CountryFactory added

--- server/app/app/Models/Country.php
+++ server/app/app/Models/Country.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Database\Factories\CountryFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -19,5 +20,10 @@ class Country extends Model
     public function resorts(): HasMany
     {
         return $this->hasMany(Resort::class);
+    }
+
+    protected static function newFactory(): CountryFactory
+    {
+        return CountryFactory::new();
     }
 }
