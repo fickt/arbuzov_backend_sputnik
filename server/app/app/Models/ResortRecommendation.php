cddc28ea025fad3097e64d60b5098c7b4d29a0d6
Michael Arbuzov <garrys2machinima@gmail.com>
2023-07-27 11:42:49 +0300

    Update ResortRecommendation.php

--- server/app/app/Models/ResortRecommendation.php
+++ server/app/app/Models/ResortRecommendation.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class ResortRecommendation extends Model
 {
     use HasFactory;
+
+    protected $table = 'user_resort_recommendations';
+    public $timestamps = false;
+
+    public $fillable = [
+        'resort_id'
+    ];
+
+    public function resort(): BelongsTo
+    {
+        return $this->belongsTo(Resort::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
